9d70d4fc8845b58762d7900294157df4b880f8f8
Daniel <danielfcarmichael@gmail.com>
2021-12-25 06:25:12 -0500

    first commit

---
+++ JavaScript/dataGeneration.js
@@ -0,0 +1,51 @@
+// import js file module
+const fs = require('fs')
+
+var xs = [];
+var ys = [];
+var zs = [];
+var allData = [];
+
+var z = 1;
+var x = 1;
+
+
+
+function generateY() {
+    for (let j = 0; j < 100; j++) {
+        var y = Math.ceil(Math.random() * 100)
+        ys.push(y)
+    }
+}
+
+function generateX() {
+    for (let j = 1; j < 101; j++) {
+        xs.push(j)
+    }
+}
+
+function generateZ() {
+    for (let j = 0; j < 100; j++) {
+        zs.push(z)
+    }
+    z++
+}
+for (let i = 0; i < 10; i++) {
+    generateZ();
+    generateX();
+    generateY();
+}
+
+allData.push(xs);
+allData.push(ys);
+allData.push(zs);
+
+//console.log(allData)
+allData = JSON.stringify(allData)
+
+try {
+    fs.writeFileSync('./JavaScript/data.js', allData)
+    //file written successfully
+  } catch (err) {
+    console.error(err)
+  }
---
+++ JavaScript/separateData.js
@@ -0,0 +1,53 @@
+// import statements
+let generatedData = require('../JavaScript/dataGeneration.js');
+var data = generatedData.data
+
+// array of array of coordinates
+// x = position in plane 1-100
+// y = random value 1-100
+// z = plane depth 1-10
+var separatedValues = [];
+var xValues = [];
+var yValues = [];
+var zValues = [];
+
+// function used to separate data into coordinates
+function coordinates() {
+    for (let item of data) {
+        // grab depth / z axis
+        var z = item[0];
+
+        // itterate to find x and y vals
+        for (let i = 1; i < item.length; i++) {
+            // currentt object in item
+            var obj = item[i]
+
+            // x and y vals as key and value from obj
+            var x = Object.keys(obj);
+            var y = Object.values(obj);
+
+            // convert x from string to int
+            x = x.map(val => parseInt(val))
+
+            // grab x and y vals from created array of key and value respectively
+            x = x[0];
+            y = y[0];
+            
+            // array to hold current x, y, and z values
+            var current = [x,y,z];
+
+            // push array of values to coordinates
+            xValues.push(x);
+            yValues.push(y);
+            zValues.push(z);
+        }
+    }
+    separatedValues.push(xValues);
+    separatedValues.push(yValues);
+    separatedValues.push(zValues);
+}
+
+coordinates();
+console.log(separatedValues)
+
+exports.
